0de26c11d0f29b083274304edd02c6603ed2990a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-04-15 20:59:09 +0200

    Agregar componente Footer con información de derechos de autor

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,18 @@
+import { Box, Container } from "@mui/material";
+
+export default function Footer () {
+    return (
+        <footer>
+            <Box sx={{ py: 3, textAlign: 'center', bgcolor: 'primary.main', color: 'white' }}>
+                <Container maxWidth="xl">
+                    <Box component="span" sx={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
+                        © 2025 Concesionarios Coches Mariano
+                    </Box>
+                    <Box component="span" sx={{ display: 'block', fontSize: '0.9rem' }}>
+                        Todos los derechos reservados.
+                    </Box>
+                </Container>
+            </Box>    
+        </footer>
+    );
+}
