2531432e559c1e84c8c9e8fb4d4454f17b19c38b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-04-15 17:06:22 +0200

    Refactor App.js para eliminar contenido innecesario y agregar enrutamiento con componentes Header, TopBar y Footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Vehiculo from './entity/Vehiculo';
+import Header from './components/layouts/Header';
+import TopBar from './components/layouts/TopBar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Lista from './pages/Lista';
+import Footer from './components/layouts/Footer';
 
 function App() {
+  const vehiculo = new Vehiculo();
+
+  console.log(vehiculo);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TopBar />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/lista' element={<Lista />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
