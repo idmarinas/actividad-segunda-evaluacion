45331eead80ad6f249c61434839233d4e45e5adb
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-04-16 14:31:55 +0200

    Corregir error tipográfico en el texto del pie de página en Footer.jsx

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer () {
             <Box sx={{ py: 3, textAlign: 'center', bgcolor: 'primary.main', color: 'white' }}>
                 <Container maxWidth="xl">
                     <Box component="span" sx={{ fontSize: '1.2rem', fontWeight: 'bold' }}>
-                        © 2025 Concesionarios Coches Mariano
+                        © 2025 Concesionario Coches Mariano
                     </Box>
                     <Box component="span" sx={{ display: 'block', fontSize: '0.9rem' }}>
                         Todos los derechos reservados.
