cc80bab25d438ead5eea3343a850d255a12f062f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-04-16 18:43:32 +0200

    Reorganizar importaciones y mejorar la estructura del componente App

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import './App.css';
-import Vehiculo from './entity/Vehiculo';
-import TopBar from './components/layouts/TopBar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Lista from './pages/Lista';
-import Footer from './components/layouts/Footer';
 import Estadisticas from './pages/Estadisticas';
-import { Container } from '@mui/material';
+import TopBar from './components/layouts/TopBar';
+import Footer from './components/layouts/Footer';
+import { Container, Paper, Typography } from '@mui/material';
+import { useTitle } from './contexts/AppContext';
 
-function App() {
-  const vehiculo = new Vehiculo();
-
-  console.log(vehiculo);
+export default function App() {
+  const { title } = useTitle();
 
   return (
     <>
       <TopBar />
+
+      <Paper elevation={3} sx={{ padding: 2, bgcolor: 'primary.light', color: 'white' }}>
+          <Typography variant="h4" component="h1" sx={{ textAlign: 'center' }}>{title}</Typography>
+      </Paper>
+      
       <Container>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/lista' element={<Lista />} />
-          <Route path='/estadisticas' element={<Estadisticas />} />
-        </Routes>
+          <Paper elevation={3} sx={{ padding: 2, marginY: 2, bgcolor: 'primary.light', color: 'white' }}>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/lista' element={<Lista />} />
+              <Route path='/estadisticas' element={<Estadisticas />} />
+            </Routes>
+          </Paper>
       </Container>
+
       <Footer />
     </>
   );
 }
-
-export default App;
