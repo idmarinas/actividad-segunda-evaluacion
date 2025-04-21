bb4eb9a46f63093c193fa8ff47e96c16dfaf250c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-04-21 22:24:22 +0200

    Eliminar lógica de título y componentes innecesarios en App para simplificar la estructura del código

--- src/App.js
+++ src/App.js
@@ -5,20 +5,14 @@ import Lista from './pages/Lista';
 import Estadisticas from './pages/Estadisticas';
 import TopBar from './components/layouts/TopBar';
 import Footer from './components/layouts/Footer';
-import { colors, Container, Paper, Typography } from '@mui/material';
-import { useTitle } from './contexts/AppContext';
+import { Container, Paper } from '@mui/material';
 
 export default function App() {
-  const { title } = useTitle();
-
   return (
     <>
       <TopBar />
       
       <Paper square component={Container} elevation={16} sx={{ padding: 2, bgcolor: 'primary.light', color: 'white', flexGrow: 1, zIndex: 100 }}>
-        <Paper elevation={0} sx={{ padding: 2, marginBottom: 2, bgcolor: colors.blue[100] }}>
-            <Typography variant="h4" component="h1" sx={{ textAlign: 'center' }}>{title}</Typography>
-        </Paper>
 
         <Routes>
           <Route path='/' element={<Home />} />
