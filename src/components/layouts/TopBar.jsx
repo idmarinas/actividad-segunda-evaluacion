8154588d625fc7ff9fc12b296050df5481ebd49b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-04-24 00:08:54 +0200

    Agregar botón para añadir vehículo y diálogo correspondiente en TopBar

--- src/components/layouts/TopBar.jsx
+++ src/components/layouts/TopBar.jsx
@@ -10,21 +10,22 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import Tooltip from '@mui/material/Tooltip';
-import SearchIcon from '@mui/icons-material/Search';
 import Logo from '../icons/Logo';
 import { Link } from 'react-router-dom';
+import { AddCircle } from '@mui/icons-material';
+import { NewVehiculoDialogForm } from '../dialogs/NewVehiculoDialog';
 
 const pages = ['Lista', 'Estadisticas'];
 
 export default function TopBar() {
     const [anchorElNav, setAnchorElNav] = useState(null);
+    const [ dialogOpen, setDialogOpen ] = useState(false);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+    const handleOpenNavMenu = (event) => setAnchorElNav(event.currentTarget);
+    const handleCloseNavMenu = () => setAnchorElNav(null);
+
+    const handleDialogOpen = () => setDialogOpen(true);
+    const handleDialogClose = () => setDialogOpen(false);
 
     return (
         <AppBar position="static" sx={{ zIndex: 200}}>
@@ -106,9 +107,10 @@ export default function TopBar() {
                             </Button>
                         ))}
                     </Box>
-                    <IconButton size="large" aria-label="search" color="inherit">
-                        <SearchIcon />
-                    </IconButton>
+                    <Button size="large" variant='contained' aria-label="Añadir vehículo" color="secondary" onClick={handleDialogOpen}>
+                        <AddCircle sx={{ marginRight: 1 }} /> Añadir Vehículo
+                    </Button>
+                    <NewVehiculoDialogForm open={dialogOpen} onClose={handleDialogClose} />
                 </Toolbar>
             </Container>
         </AppBar>
