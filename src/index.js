4ba9fe3a16ae6bb3235f08fdf1b14d7e930840df
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-04-27 19:12:05 +0200

    Eliminar el proveedor TitleProvider del archivo index.js para simplificar la estructura del código

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { TitleProvider } from './contexts/AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <TitleProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </TitleProvider>
   </React.StrictMode>
 );
 
