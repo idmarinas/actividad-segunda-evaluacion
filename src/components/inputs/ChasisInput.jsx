cfc8b88313c9927098eb46c30eea31c21abf4f9c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-04-29 20:52:28 +0200

    Agregar id y listado a la función handleChange en ChasisInput para mejorar la validación del chasis

--- src/components/inputs/ChasisInput.jsx
+++ src/components/inputs/ChasisInput.jsx
@@ -1,15 +1,17 @@
 import { TextField } from "@mui/material";
 import { useState } from "react";
 import { useForm } from "../../contexts/FormContext";
+import { useVehiculos } from "../../contexts/AppContext";
 
 export default function ChasisInput(props) {
-    const { pending, ...attrs } = props;
+    const { pending, id, ...attrs } = props;
     const [error, setError] = useState(false);
     const [helpText, setHelpText] = useState("");
     const [color, setColor] = useState('');
+    const { listado } = useVehiculos();
     const form = useForm();
 
-    const handleChange = (event) => form.validateChasis(event.target.value, setError, setHelpText, form.setFormValid, setColor);
+    const handleChange = (event) => form.validateChasis(event.target.value, setError, setHelpText, form.setFormValid, setColor, id, listado);
 
     return (
         <>
